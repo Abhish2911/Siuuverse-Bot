e95e2659e4a01510b4290ffff869357374248988
Abhish2911 <92803639+Abhish2911@users.noreply.github.com>
2026-07-14 17:22:25 +0530

    Update player stats formatting in myrpclub.js

--- commands/myrpclub.js
+++ commands/myrpclub.js
@@ -142,7 +142,7 @@ module.exports = {
         const mv = row[3] || '0';
         const tp = row[16] || '0';
 
-        return `\`${startIndex + index}.\` **${name}**\n> OVR ${ovr}   MV ${mv}   TP ${tp}`;
+        return `\`${startIndex + index}.\` **${name}**\n> ${emojis.rank} ${ovr}  💸 ${mv}   ${emojis.stats} ${tp}`;
       })
       .join('\n');
 
