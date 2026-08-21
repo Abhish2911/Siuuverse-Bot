381e8520c51042eee097084922f04ac551869001
Abhish2911 <92803639+Abhish2911@users.noreply.github.com>
2026-08-21 12:30:39 +0530

    Add files via upload

--- prefix-commands/hflock.js
+++ prefix-commands/hflock.js
@@ -16,7 +16,7 @@ module.exports = {
 
     try {
       const role = await setLocked(message.channel, true, `Locked by ${message.author.tag}`);
-      return message.reply(`${E.lock} <@&${role.id}> can no longer send messages in this channel.`);
+      return message.reply(`${E.lock} The configured HF lock role can no longer send messages here and is now non-mentionable.`);
     } catch (error) {
       return message.reply(`${E.wrong} ${error.message}`);
     }
--- prefix-commands/hfunlock.js
+++ prefix-commands/hfunlock.js
@@ -16,7 +16,7 @@ module.exports = {
 
     try {
       const role = await setLocked(message.channel, false, `Unlocked by ${message.author.tag}`);
-      return message.reply(`${E.correct} <@&${role.id}> can send messages in this channel again.`);
+      return message.reply(`${E.correct} The configured HF lock role can send messages here again and is now mentionable.`);
     } catch (error) {
       return message.reply(`${E.wrong} ${error.message}`);
     }
